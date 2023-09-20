d625a897d5deebf1f84f8b394a6d9e62b9da0c1a
litzyDedica <litzy.reyes@dedica.com.mx>
2023-09-20 12:00:47 -0500

    New features to the login

--- Verbum-bijih/login.js
+++ Verbum-bijih/login.js
@@ -2,11 +2,15 @@ const predefinedPassword = "$t682E2WGVf35fVGQ*^ZM7Pio";
 
 function checkPassword() {
   const enteredPassword = document.getElementById("password").value;
-
   if (enteredPassword === predefinedPassword) {
     document.getElementById("password-prompt").style.display = "none";
     document.getElementById("app-content").style.display = "block";
+    localStorage.setItem("session", JSON.stringify("guest"));
+
   } else {
-    alert("Incorrect password. Please try again.");
+    Swal.fire({
+      title: "<strong>Incorrect password. </u></strong>",
+      icon: "error",
+    });
   }
 }
