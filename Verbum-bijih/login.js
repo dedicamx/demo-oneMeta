0c0fac3849712c86ade307275551dc03b592a862
litzyDedica <litzy.reyes@dedica.com.mx>
2023-09-20 12:51:23 -0500

    Add sessions

--- Verbum-bijih/login.js
+++ Verbum-bijih/login.js
@@ -2,15 +2,25 @@ const predefinedPassword = "$t682E2WGVf35fVGQ*^ZM7Pio";
 
 function checkPassword() {
   const enteredPassword = document.getElementById("password").value;
+  const currentTime = new Date().getTime();
+  console.log(currentTime);
+  const sessionExpirationTime = currentTime + 60 * 1000;
+
   if (enteredPassword === predefinedPassword) {
     document.getElementById("password-prompt").style.display = "none";
     document.getElementById("app-content").style.display = "block";
-    localStorage.setItem("session", JSON.stringify("guest"));
-
+    localStorage.setItem(
+      "session",
+      JSON.stringify({
+        username: "guest",
+        expirationTime: sessionExpirationTime,
+      })
+    );
   } else {
     Swal.fire({
       title: "<strong>Incorrect password. </u></strong>",
       icon: "error",
+      confirmButtonColor: '#007bff',
     });
   }
 }
